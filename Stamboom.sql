1a3dc52395cb2d2e03dc3369126c7dfab743b2ab
Maxime de Lange <maximedelange1991@hotmail.com>
2016-03-15 11:49:53 +0100

    SQL script 1.0
    Verbeterde versie

--- Stamboom.sql
+++ Stamboom.sql
@@ -1,9 +1,7 @@
-create database Stamboom;
-
 create table Persoon
 (
 	id int primary key,
-	naam varchar(100),
+	naam varchar(100)
 );
 
 create table Gezin
@@ -11,12 +9,12 @@ create table Gezin
 	id int primary key,
 	persoon_id int references persoon(id),
 	ouder1 varchar(100),
-	ouder2 varchar(100),
+	ouder2 varchar(100)
 );
 
 create table Familie
 (
 	id int primary key,
-	gezin_id int references gezin(id)
+	gezin_id int references gezin(id),
 	naam varchar(100)
 );
