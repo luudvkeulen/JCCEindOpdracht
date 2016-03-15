f5b81cdf837e3aeaf6411227a2fe0763a6aebf73
Luud van Keulen <l.vankeulen@student.fontys.nl>
2016-03-15 11:16:42 +0100

    Added jdbc

--- src/jcceindopdracht/FXMLController.java
+++ src/jcceindopdracht/FXMLController.java
@@ -4,21 +4,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author Luud
- */
 public class FXMLController implements Initializable
 {
-
-	/**
-	 * Initializes the controller class.
-	 */
 	@Override
 	public void initialize(URL url, ResourceBundle rb)
 	{
-		// TODO
-	}	
-	
+
+	}		
 }
