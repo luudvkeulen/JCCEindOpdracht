2de26a42599a75e4d166b07ab8c87e26e40d02f6
Luud van Keulen <l.vankeulen@student.fontys.nl>
2016-03-15 11:50:53 +0100

    Database executes

--- src/jcceindopdracht/db/Database.java
+++ src/jcceindopdracht/db/Database.java
@@ -1,6 +1,10 @@
 package jcceindopdracht.db;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,7 +24,7 @@ public class Database
 			return connection;
 		} catch (SQLException e)
 		{
-			Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, e.getMessage());
+			Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, e);
 			return null;
 		}
 	}
@@ -33,10 +37,34 @@ public class Database
 			return true;
 		} catch (SQLException e)
 		{
-			Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, e.getMessage());
+			Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, e);
 			return false;
 		}	
 	}
+
+	public int executeNonQuery(String query)
+	{
+		try
+		{
+			Statement statement = connection.createStatement();	
+			return statement.executeUpdate(query);
+		} catch (SQLException ex)
+		{
+			Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+			return 0;
+		}
+	}
 	
-	
+	public ResultSet executeQuery(String query)
+	{
+		try
+		{
+			Statement statement = connection.createStatement();		
+			return statement.executeQuery(query);
+		} catch (SQLException ex)
+		{
+			Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+			return null;
+		}
+	}
 }
